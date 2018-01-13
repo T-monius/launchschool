33d458ec0288454ada05d501592dcc6374051a44
Torrel Moseley <trrl.mo@hotmail.com>
2018-01-12 17:07:53 -0700

    Lesson 6 - More

--- 101/lesson_6/twenty_one/calculate_values.rb
+++ 101/lesson_6/twenty_one/calculate_values.rb
@@ -7,12 +7,64 @@
 #   calculate_values
 #     Separate aces into hash
 #     total variable equal to total value of non-ace cards
-#     Call calculate_aces on total and aces
+#     Call calculate_aces with arguments total and aces
 #     calculate_aces
 #       Two parameters: total and aces hash
 #       Can't have more than one ace that equals 11
 #       Value of first ace most important
 #       If more than 2 aces, they all have to equal 1
-#       if total + one ace < 21 first ace equals 11
+#       Operate on index of ace value array? Or, simply return appropriate
+#       number?
+#       Set aces_present = aces_hash.size
+#       Set ace_keys = aces_hash.keys
+#       case aces_present
+#       when 0 then return total
+#       when 1
+#         if total + aces_hash[aces_keys[0]][1] <= 21
+#           total + 11
+#         else
+#           total += 1
+#       when 2
+#         if total + aces_hash[ace_keys[0]][1] + aces_hash[ace_keys[1]][0] <= 21
+#            total + aces_hash[ace_keys[0]][1] + aces_hash[ace_keys[1]][0]
+#         else
+#           total + aces_hash[ace_keys[0]][0] + aces_hash[ace_keys[1]][0]
+#       when aces_present > 2
+#         aces_present.times { total += 1 }
 #     Return total
-#     
+
+# Separate out aces
+#   Have a hash iterate over hash
+#   Set aces_hash to Select value is_a?(array)
+#   Call deleteif on hnd if value is_a?(Array)
+
+def calculate_aces(a_hsh, total)
+  aces_present = a_hsh.size
+  ace_keys = a_hsh.keys
+
+  case aces_present
+  when 0 then return total
+  when 1 
+    if total + 11 <= 21
+      total += 11
+    else
+      total += 1
+    end
+  when 2
+    if total + 12 <= 21
+      total += 12
+    else
+      total += 2
+    end
+  else
+    total += aces_present
+  end
+  total
+end
+
+def calculate_values(hnd)
+  aces_hash = hnd.select { |crd, value| value.is_a?(Array) }
+  hnd.delete_if { |crd, value| value.is_a?(Array) }
+  total = hnd.values.sum
+  calculate_aces(aces_hash, total)
+end
---
+++ 101/lesson_6/twenty_one/dealer_turn.rb
@@ -0,0 +1,7 @@
+# dealer_turn.rb
+
+# The dealer turn will follow a very similar pattern as the player
+# turn. Except: the dealer's break condition will occur at the top of
+# the "hit or stay" loop. See if you can figure out why that is.
+
+
