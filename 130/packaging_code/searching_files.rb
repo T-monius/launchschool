dbe0622bc8e6c7945dbb1007ec0f131704801ba7
T Mo <trrl.mo@hotmail.com>
2018-09-12 14:44:57 +0300

    Searching Files Exercise

---
+++ 130/packaging_code/searching_files.rb
@@ -0,0 +1,9 @@
+# searching_files.rb
+
+desc 'Display inventory of all files'
+task :inventory do
+  Find.find('.') do |name|
+    next if name.include?('/.') # Excludes files and directories with . names
+    puts name if File.file?(name)
+  end
+end
