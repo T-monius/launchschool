17f8410d6fb513ea816025a38a2c05f8b3807477
T Mo <trrl.mo@hotmail.com>
2018-12-27 09:52:16 -0700

    Small Problems Easy 6: 6 further exploration

--- 101/exercise_rework/easy_6/combining_arrays.rb
+++ 101/exercise_rework/easy_6/combining_arrays.rb
@@ -4,8 +4,19 @@ def merge(arr, arr1)
   arr | arr1
 end
 
-def fe_merge(arr, arr1)
-  [arr, arr1]
+def keep_iterating?(arr, arr1, idx)
+  arr.fetch(idx, 'nope') != 'nope' || arr1.fetch(idx, 'nope') != 'nope'
 end
 
-p fe_merge([1, 3, 5], [3, 6, 9])
+def fe_merge(arr, arr1)
+  new_arr = []
+  idx = 0
+  while keep_iterating?(arr, arr1, idx)
+    element = arr.fetch(idx, 'nope') unless arr.fetch(idx, 'nope') == 'nope'
+    element1 = arr1.fetch(idx, 'nope') unless arr1.fetch(idx, 'nope') == 'nope'
+    new_arr << element unless new_arr.include?(element)
+    new_arr << element1 unless new_arr.include?(element1)
+    idx += 1
+  end
+  new_arr.sort
+end
--- 101/exercise_rework/easy_6/test/combining_arrays_test.rb
+++ 101/exercise_rework/easy_6/test/combining_arrays_test.rb
@@ -9,4 +9,12 @@ class CombiningArraysTest < Minitest::Test
   def test_two_inputs
     assert_equal([1, 3, 5, 6, 9], merge([1, 3, 5], [3, 6, 9]))
   end
+
+  def test_further_exploration
+    assert_equal([1, 3, 5, 6, 9], fe_merge([1, 3, 5], [3, 6, 9]))
+  end
+
+  def test_further_exploration_with_arrays_of_unequal_length
+    assert_equal([1, 3, 5, 6, 7, 9], fe_merge([1, 3, 5, 7], [3, 6, 9]))
+  end
 end
