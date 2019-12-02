6c2eac5c430d0e1c3b9936e68b28be36437147a1
mo mac <trrl.mo@hotmail.com>
2019-12-02 12:47:02 -0700

    JS225 Exercises - Object Creation Patterns: 4

--- 225/exercises/objectCreationPatterns/classicalWithMixin.js
+++ 225/exercises/objectCreationPatterns/classicalWithMixin.js
@@ -1,4 +1,4 @@
-// classicalObjectCreation.js
+// classicalWithMixin.js
 
 function Person(firstName, lastName, age, gender) {
   this.firstName = firstName;
@@ -72,47 +72,48 @@ GraduateStudent.prototype.research = function () {
   console.log('Researching');
 }
 
-console.log('Person:')
-var person = new Person('foo', 'bar', 21, 'gender');
-console.log(person instanceof Person);     // logs true
-person.eat();                              // logs 'Eating'
-person.communicate();                      // logs 'Communicating'
-person.sleep();                            // logs 'Sleeping'
-console.log(person.fullName());            // logs 'foo bar'
+// Begin addding Mixin
+function extend(targetObject, sourceObject) {
+  for(prop in sourceObject) {
+    targetObject[prop] = sourceObject[prop];
+  }
+  return targetObject;
+}
 
-console.log('-----------------------');
-console.log('Doctor:');
-var doctor = new Doctor('foo', 'bar', 21, 'gender', 'Pediatrics');
+var professional = {
+  payTax: function () {
+    console.log(this.fullName() + ' Paying taxes')
+  },
+  invoice: function() {
+    console.log(this.fullName() + ' is Billing customer');
+  },
+};
+
+var doctor = extend(new Doctor('foo', 'bar', 21, 'gender', 'Pediatrics'), professional);
 console.log(doctor instanceof Person);     // logs true
 console.log(doctor instanceof Doctor);     // logs true
 doctor.eat();                              // logs 'Eating'
 doctor.communicate();                      // logs 'Communicating'
 doctor.sleep();                            // logs 'Sleeping'
-console.log(doctor.fullName());            // logs 'foo bar'
+console.log(doctor.fullName());            // logs 'foo bar'S
 doctor.diagnose();                         // logs 'Diagnosing'
 
-console.log('-----------------------');
-console.log('Graduate Student:');
-var graduateStudent = new GraduateStudent('foo', 'bar', 21, 'gender', 'BS Industrial Engineering', 'MS Industrial Engineering');
-// logs true for next three statements
-console.log(graduateStudent instanceof Person);
-console.log(graduateStudent instanceof Student); 
-console.log(graduateStudent instanceof GraduateStudent);
-graduateStudent.eat();                     // logs 'Eating'
-graduateStudent.communicate();             // logs 'Communicating'
-graduateStudent.sleep();                   // logs 'Sleeping'
-console.log(graduateStudent.fullName());   // logs 'foo bar'
-graduateStudent.study();                   // logs 'Studying'
-graduateStudent.research();                // logs 'Researching'
+var professor = extend(new Professor('foo', 'bar', 21, 'gender', 'Systems Engineering'), professional);
+console.log(professor instanceof Person);     // logs true
+console.log(professor instanceof Professor);  // logs true
+professor.eat();                              // logs 'Eating'
+professor.communicate();                      // logs 'Communicating'
+professor.sleep();                            // logs 'Sleeping'
+console.log(professor.fullName());            // logs 'foo bar'
+professor.teach();                            // logs 'Teaching'
 
-console.log('----------------------');
-console.log('Professor:')
-var professor = new Professor('Rod', 'Stuart', 48, 'male', 'Musicology');
-graduateStudent.eat();                     // logs 'Eating'
-graduateStudent.communicate();             // logs 'Communicating'
-graduateStudent.sleep();                   // logs 'Sleeping'
-console.log(graduateStudent.fullName());   // logs 'foo bar'
-professor.teach();                         // logs 'Teaching'
-console.log(professor.subject)             // logs 'Musicology'
-console.log(professor instanceof Person);
-console.log(professor instanceof Professor);
+doctor.invoice();                          // logs 'foo bar is Billing customer'
+doctor.payTax();                           // logs 'foo bar Paying taxes'
+
+professional.invoice = function() {
+  console.log(this.fullName() + ' is Asking customer to pay');
+};
+
+doctor.invoice();                          // logs 'foo bar is Asking customer to pay'
+professor.invoice();                       // logs 'foo bar is Asking customer to pay'
+professor.payTax();                        // logs 'foo bar Paying taxes'
