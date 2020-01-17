81c035b900e6248c96c2635175cadca5878d6778
mo mac <trrl.mo@hotmail.com>
2020-01-17 16:48:13 -0500

    JS230 Lesson 2: DOM Exercises > Events and Asynchronous Programming > 6, partway

--- 230/lesson2/DOMExercises/selectionFilters.js
+++ 230/lesson2/DOMExercises/selectionFilters.js
@@ -1,2 +1,40 @@
 // selectionFilters.js
 
+var classifications = {
+  all: ['Classifications', 'Vertebrate', 'Warm-blooded', 'Cold-blodded', 'Mammal', 'Bird' ],
+  Bear: ['Vertebrate', 'Warm-blooded', 'Mammal'],
+  Turtle: ['Vertebrate', 'Cold-blooded'],
+  Whale: ['Vertebrate', 'Warm-blooded', 'Mammal'],
+  Salmon: ['Vertebrate', 'Cold-blooded'],
+  Ostrich: ['Vertebrate', 'Warm-blooded', 'Bird'],
+};
+
+var animals = {
+  all: ['Animals', 'Bear', 'Turtle', 'Whale', 'Salmon', 'Ostrich'],
+  Vertebrate: ['Bear', 'Turtle', 'Whale', 'Salmon', 'Ostrich'],
+  Warm-blooded: ['Bear', 'Whale', 'Ostrich'],
+  Cold-blooded: ['Salmon', 'Turtle'],
+  Mammal: ['Bear', 'Whale'],
+  Bird: ['Ostrich'],
+};
+
+function teardownSelectBox(boxSelector) {
+  var selectBox = document.querySelector(boxSelector);
+
+  Array.from(selectBox.children).forEach( function(child) {
+    child.remove();
+  });
+
+  undefined;
+}
+
+var classificationNodes = document.querySelector('#animal-classifications').children;
+classificationNodes = Array.from(classificationNodes, function(childEl) {
+  childEle.cloneNode();
+});
+
+var animalNodes = document.querySelector('#animals').children;
+animalNodes = Array.from(animalNodes, function(childEl) {
+  childEle.cloneNode();
+});
+
